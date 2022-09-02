4be4f543362fb9027f81d94fe528abacf8355fca
Christopher Casey <chrisacas20@gmail.com>
2022-09-02 13:35:37 -0500

    feature: format dataframe correct

--- ImportData.py
+++ ImportData.py
@@ -1,8 +1,14 @@
 import numpy as np
 import pandas as pd
 
-def import_file(filename):
-    dataframe = pd.read_csv(filename)
+def import_col_names(filename):
+    with open(filename) as file:
+        col_names = file.readlines()
+        return [col_name.split(':', 1)[0] for col_name in col_names]
+    
+
+def import_file(filename, col_names):
+    dataframe = pd.read_csv(filename, names=col_names)
     return dataframe
 
 def print_dataframe(dataframe):
@@ -14,13 +20,21 @@ def num_instances(dataframe):
 def num_columns(dataframe):
     return len(dataframe.columns)
 
+
 def main():
     filename = "census-income.data"
-    dataframe = import_file(filename)
+    col_names = "census-income-col_names.txt"
+    col_names = import_col_names(col_names)
+    
+
+    print(len(col_names))
+    print(col_names)
+    dataframe = import_file(filename, col_names)
 
     print("Rows: ", num_instances(dataframe))
     print("Columns: ", num_columns(dataframe))
-    print_dataframe(dataframe)
+    dataframe.head().to_csv("export.csv")
+    
 
 if __name__ == "__main__":
     main()
