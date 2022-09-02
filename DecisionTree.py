29cb4749952b68afe1eea99518e776a0704c0e34
Christopher Casey <chrisacas20@gmail.com>
2022-09-02 15:22:56 -0500

    chore: add packages for decision tree

---
+++ DecisionTree.py
@@ -0,0 +1,12 @@
+from sklearn.tree import DecisionTreeClassifier
+from sklearn import metrics
+import pandas as pd
+import numpy as np
+
+
+def main():
+    print("main")
+    
+
+if __name__ == "__main__":
+    main()
--- ImportData.py
+++ ImportData.py
@@ -6,6 +6,8 @@ def import_col_names(filename):
         col_names = file.readlines()
         return [col_name.split(':', 1)[0] for col_name in col_names]
     
+def export_dataframe(dataframe, filename="export.csv"):
+    dataframe.head().to_csv(filename)
 
 def import_file(filename, col_names):
     dataframe = pd.read_csv(filename, names=col_names)
@@ -20,20 +22,36 @@ def num_instances(dataframe):
 def num_columns(dataframe):
     return len(dataframe.columns)
 
+def get_training_set():
+    training_filename = "census-income.data"
+    col_names_file = "census-income-col_names.txt"
+    col_names = import_col_names(col_names_file)
+    training_dataframe = import_file(training_filename, col_names)
+    return training_dataframe
+
+def get_testing_set():
+    test_filename = "census-income.test"
+    col_names_file = "census-income-col_names.txt"
+    col_names = import_col_names(col_names_file)
+    testing_dataframe = import_file(test_filename, col_names)
+    return testing_dataframe
+
+
 
 def main():
-    filename = "census-income.data"
-    col_names = "census-income-col_names.txt"
-    col_names = import_col_names(col_names)
+    training_filename = "census-income.data"
+    test_filename = "census-income.test"
+    col_names_file = "census-income-col_names.txt"
     
+    col_names = import_col_names(col_names_file)
+    
+    training_dataframe = import_file(training_filename, col_names)
+    print("Training Rows: ", num_instances(training_dataframe))
+    print("Training Columns: ", num_columns(training_dataframe))
 
-    print(len(col_names))
-    print(col_names)
-    dataframe = import_file(filename, col_names)
-
-    print("Rows: ", num_instances(dataframe))
-    print("Columns: ", num_columns(dataframe))
-    dataframe.head().to_csv("export.csv")
+    testing_dataframe = import_file(test_filename, col_names)
+    print("Training Rows: ", num_instances(testing_dataframe))
+    print("Training Columns: ", num_columns(testing_dataframe))
     
 
 if __name__ == "__main__":
